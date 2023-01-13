dde839681b9ab0141a00ab538e63b9a9d8f8feee
Pinak Mehta <pinak@moweb.com>
2023-01-13 15:51:16 +0530

    feat: print variable value and it's datatype

---
+++ 03_variables.py
@@ -0,0 +1,11 @@
+numberVariable = 100
+print("numberVariable ===>>> ", numberVariable)
+print("numberVariable type ===>>> ", type(numberVariable))
+
+stringVariable = "Pinak Mehta"
+print("stringVariable ===>>> ", stringVariable)
+print("stringVariable type ===>>> ", type(stringVariable))
+
+floatingVariable = 10.25
+print("floatingVariable ===>>> ", floatingVariable)
+print("floatingVariable type ===>>> ", type(floatingVariable))
