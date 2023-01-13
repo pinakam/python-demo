b4f0dbaf2d6f5b54495738986810919ac9d41fbe
Pinak Mehta <pinak@moweb.com>
2023-01-13 17:51:56 +0530

    feat: create dictionary and use it's methods

---
+++ 07_dictionary.py
@@ -0,0 +1,8 @@
+dictionary = {
+    'name': 'Pinak Mehta',
+    'age': 29
+}
+
+print("dictionary ===>>> ", dictionary)
+print("dictionary items ===>>> ", dictionary.items())
+print("dictionary keys ===>>> ", dictionary.keys())
