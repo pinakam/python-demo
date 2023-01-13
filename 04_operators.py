25a894fb2ca30d19a3f9b05788bd98630be954c7
Pinak Mehta <pinak@moweb.com>
2023-01-13 16:04:40 +0530

    feat: Use Arithmetic and Assignment operators

---
+++ 04_operators.py
@@ -0,0 +1,28 @@
+# ================ Arithmetic & Assignment Operators==================
+a = 30  # = is the assignment operator
+b = 20
+
+# Addition
+c = a + b
+print("c (Addition)===>>> ", c)
+
+# Substation
+c = a - b
+print("c (Substation)===>>> ", c)
+
+# Multiplication
+c = a * b
+print("c (Multiplication)===>>> ", c)
+
+# Division
+c = a / b
+print("c (Division)===>>> ", c)
+
+# Reminder
+c = a % b
+print("c (Reminder)===>>> ", c)
+
+# += is shorthand to add value in variable value
+print("Before += a is ===>>> ", a)
+a += 5
+print("After += a is ===>>> ", a)
