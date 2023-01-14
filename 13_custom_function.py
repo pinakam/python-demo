97564970000071ae4006131430df7420457b558a
Pinak Mehta <pinak@moweb.com>
2023-01-14 13:48:58 +0530

    feat: create a demo of custom user define function

---
+++ 13_custom_function.py
@@ -0,0 +1,8 @@
+def addition(var1, var2):
+    return int(var1) + int(var2)
+
+
+a = input("Enter value for the A:-> ")
+b = input("Enter value for the B:-> ")
+
+print("Addition of a and b is: -->> ", addition(a, b))
