d00bc795bb3c9acccd1b4e1223c4e8817a49dc62
Pinak Mehta <pinak@moweb.com>
2023-01-14 13:38:46 +0530

    feat: create a demo of the for loop

---
+++ 11_for_loop.py
@@ -0,0 +1,21 @@
+products = [
+    {
+        "id": 1,
+        "name": "Product 1",
+        "price": 100
+    },
+    {
+        "id": 2,
+        "name": "Product 2",
+        "price": 200
+    },
+    {
+        "id": 3,
+        "name": "Product 3",
+        "price": 300
+    }
+]
+
+for product in products:
+    print("Id: -> ", product['id'], " Name: -> ", product['name'], " Price :-> ", product['price'])
+    print('')
